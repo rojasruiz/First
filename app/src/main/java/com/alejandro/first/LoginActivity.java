87baee6910717ae0e347b212f02ffd405d4e5e72
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2021-10-21 09:57:21 +0200

    Added butons and functionality button

---
+++ app/src/main/java/com/alejandro/first/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.alejandro.first;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * @author Alejandro
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
