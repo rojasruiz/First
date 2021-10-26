a79a7e783fe0bd8d4c208579aba0339734db7378
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2021-10-26 09:50:57 +0200

    Splash screen and register screen added

--- app/src/main/java/com/alejandro/first/LoginActivity.java
+++ app/src/main/java/com/alejandro/first/LoginActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 
 /**
  * @author Alejandro
@@ -19,10 +22,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Glide for loading girls
+        ImageView mGirl= findViewById(R.id.girl);
+
+        Glide.with(this)
+                //.load("https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80")
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                //centerCrop();
+                //circleCrop();
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.blue_500)))
+                .into(mGirl);
     }
 
-    public void onClick(View view) {
+    public void btnOpenMain(View view) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void btnRegister(View view) {
+        Intent intent = new Intent(LoginActivity.this, SignUp.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/alejandro/first/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.alejandro.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    public void btnOpenLogin(View view) {
+        Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
