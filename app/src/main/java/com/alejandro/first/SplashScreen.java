8cb3d5809ad168c8fbee382fb36fc9ce62fe83e1
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2021-11-04 08:50:38 +0100

    Splash screen updated and animations added

--- app/src/main/java/com/alejandro/first/SplashScreen.java
+++ app/src/main/java/com/alejandro/first/SplashScreen.java
@@ -4,20 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        logo = (ImageView) findViewById(R.id.logoSplash);
+        Animation scale = AnimationUtils.loadAnimation(this, R.anim.scale_animation);
+        logo.startAnimation(scale);
+
+        openApp(true);
+    }
+
+    private void openApp(boolean LocationPermision){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent1 = new Intent(SplashScreen.this, LoginActivity.class);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent1);
+            }
+        },1100);
     }
 
     public void btnOpenLogin(View view) {
-        Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
     }
 }
